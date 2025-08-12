78a2f010e1a48c9a3b07a3c7c6a5339cde847f1a
sy <stroudafk@gmail.com>
2025-08-11 20:34:41 -0500

    unicorne | update layers, add gaming layer

--- keyboards/boardsource/unicorne/keymaps/sy/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/sy/keymap.c
@@ -2,57 +2,70 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // colemak
-    [0] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_BSPC,
-                            LCTL_T(KC_ESC), LCTL_T(KC_A), LALT_T(KC_R), LGUI_T(KC_S), LSFT_T(KC_T), KC_G, KC_M, RSFT_T(KC_N), RGUI_T(KC_E), RALT_T(KC_I), RCTL_T(KC_O), KC_QUOT,
-                            KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT),
-                            KC_LGUI, MO(3), KC_BSPC, KC_SPC, MO(4), KC_RALT
+    [0] = LAYOUT_split_3x6_3(KC_TAB, KC_J, KC_L, KC_Y, KC_P, KC_K, KC_Z, KC_F, KC_O, KC_U, KC_QUOT, KC_BSPC,
+                            LCTL_T(KC_ESC), LCTL_T(KC_C), LALT_T(KC_R), LGUI_T(KC_S), LSFT_T(KC_T), KC_G, KC_M, RSFT_T(KC_N), RGUI_T(KC_E), RALT_T(KC_I), RCTL_T(KC_A), KC_ENT,
+                            KC_LSFT, KC_W, KC_Q, KC_V, KC_D, KC_B, KC_X, KC_H, KC_SLSH, KC_COMM, KC_DOT, KC_RSFT,
+                            KC_ESC, MO(2), KC_BSPC, KC_SPC, MO(3), KC_ENT
                             ),
     // qwerty
     [1] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-                            LCTL_T(KC_ESC), LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, RSFT_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
-                            KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_M, KC_N, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT),
-                            KC_LGUI, MO(3), KC_BSPC, KC_SPC, MO(4), KC_RALT
-                            ),
-    // gaming
-    // TODO: add gaming num layer
-    [2] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
                             LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
                             KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_M, KC_N, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT),
-                            KC_RALT, MO(3), KC_SPC, KC_SPC, MO(4), KC_LGUI
+                            KC_RALT, MO(4), KC_SPC, KC_SPC, MO(3), KC_LGUI
                             ),
     // symbol
-    [3] = LAYOUT_split_3x6_3(_______, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, KC_TILDE, KC_PIPE, KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK, _______,
-                            _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_QUOTE, KC_DQT, _______,
+    [2] = LAYOUT_split_3x6_3(_______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_TILDE, KC_GRV, _______,
+                            _______, KC_LABK, KC_LBRC, KC_LCBR, KC_LPRN, KC_BSLS, KC_PIPE, KC_RPRN, KC_RCBR, KC_RBRC, KC_RABK, _______,
                             _______, KC_BSLS, KC_TILDE, KC_GRV, KC_COLN, KC_SCLN, KC_UNDS, KC_MINUS, KC_PLUS, KC_EQL, KC_QUES, _______,
                             _______, _______, _______, KC_ESC, KC_TAB, _______),
     // nav
-    [4] = LAYOUT_split_3x6_3(_______, KC_MINUS, KC_7, KC_8, KC_9, KC_SLSH, KC_PGUP, KC_HOME, KC_UP, KC_END, OSL(5),_______,
+    [3] = LAYOUT_split_3x6_3(_______, KC_MINUS, KC_7, KC_8, KC_9, KC_SLSH, KC_PGUP, KC_HOME, KC_UP, KC_END, KC_INS,_______,
                             _______, KC_MINUS, KC_4, KC_5, KC_6, KC_ASTR, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL, _______,
-                            _______, KC_PLUS, KC_1, KC_2, KC_3, KC_TAB, KC_CAPS, KC_ESC, CW_TOGG, KC_RSFT, KC_PSCR, _______,
+                            _______, KC_PLUS, KC_1, KC_2, KC_3, KC_TAB, KC_PSCR, CW_TOGG, KC_CAPS, KC_RSFT, OSL(5), _______,
                             _______, KC_0, KC_ENT, _______, _______, _______),
+    // gaming num - it functions with esdf.
+    [4] = LAYOUT_split_3x6_3(_______, KC_1, KC_2,_______, KC_3, KC_4,_______,_______,_______,_______,_______,_______,
+                            _______, KC_6, _______, _______, _______, KC_5,_______,_______,_______,_______,_______,_______,
+                            _______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+                            _______,_______,_______,_______,_______,_______),
     // fn
     // TODO: add toggle gaming layer and qwerty
-    [5] = LAYOUT_split_3x6_3(RGB_TOG, _______,_______,_______,_______,_______,_______,_______,_______,KC_F11,KC_F12,_______,
-                            _______, KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_F7,KC_F8,KC_F9, KC_F10,_______,
-                            _______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
-                            _______,_______,_______,_______,_______,_______)
+    [5] = LAYOUT_split_3x6_3(RGB_TOG, KC_F12,KC_F7, KC_F8, KC_F9,_______,_______,_______,_______, TG(1),_______,_______,
+                            _______, KC_F11, KC_F4, KC_F5, KC_F6,_______,_______,_______,_______,_______,_______,_______,
+                            _______, KC_F10, KC_F1, KC_F2, KC_F3,_______,_______,_______,_______,_______,_______,_______,
+                            _______,_______,_______,_______,_______,_______),
     // [_GAMING] = LAYOUT_split_3x6_3(),
     // [_SYMBOL] = LAYOUT_split_3x6_3(),
     // [_NAVIGATION] = LAYOUT_split_3x6_3(),
     // [_FN] = LAYOUT_split_3x6_3()
+
+    // blank layer
+    // [] = LAYOUT_split_3x6_3(_______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+    //                         _______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+    //                         _______, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+    //                         _______,_______,_______,_______,_______,_______)
+    //                         ),
 };
 
-const uint16_t PROGMEM esc_combo[] = {KC_X, KC_C, COMBO_END};
-const uint16_t PROGMEM enter_combo[] = {KC_COMM, KC_DOT, COMBO_END};
+// todo: when I'm not being lazy, change this to be only on fletchling layer
+// const uint16_t PROGMEM esc_combo[] = {KC_L, KC_Y, COMBO_END};
+const uint16_t PROGMEM enter_combo[] = {KC_SLSH, KC_COMMA, COMBO_END};
 
 combo_t key_combos[] = {
-    COMBO(esc_combo, KC_ESC),
+//     COMBO(esc_combo, KC_ESC),
     COMBO(enter_combo, KC_ENT),
 };
 
 const key_override_t del_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
 const key_override_t ltab_override = ko_make_basic(MOD_BIT(KC_LSFT), KC_SPC, KC_TAB);
 const key_override_t rtab_override = ko_make_basic(MOD_BIT(KC_RSFT), KC_SPC, KC_TAB);
+
+const key_override_t lcomma_override = ko_make_basic(MOD_BIT(KC_LSFT), KC_COMM, KC_SCLN);
+const key_override_t rcomma_override = ko_make_basic(MOD_BIT(KC_RSFT), KC_COMM, KC_SCLN);
+
+const key_override_t ldot_override = ko_make_basic(MOD_BIT(KC_LSFT), KC_DOT, KC_COLN);
+const key_override_t rdot_override = ko_make_basic(MOD_BIT(KC_RSFT), KC_DOT, KC_COLN);
 // const key_override_t tab_override = {.replacement      = KC_TAB,
 //                                     .trigger           = KC_SPACE,
 //                                     .trigger_mods      = MOD_BIT(KC_LSFT),
@@ -62,7 +75,11 @@ const key_override_t rtab_override = ko_make_basic(MOD_BIT(KC_RSFT), KC_SPC, KC_
 const key_override_t *key_overrides[] = {
     &del_override,
     &ltab_override,
-    &rtab_override
+    &rtab_override,
+    &lcomma_override,
+    &rcomma_override,
+    &ldot_override,
+    &rdot_override,
 };
 
 bool is_flow_tap_key(uint16_t keycode) {
