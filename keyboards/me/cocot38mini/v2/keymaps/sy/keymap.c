b3d6be2d9296fe1e3b97c85c666a53e3594a4b3f
sy stroud <stroudafk@gmail.com>
2026-08-12 19:42:05 -0500

    removed permissive hold from long press

--- keyboards/me/cocot38mini/v2/keymaps/sy/keymap.c
+++ keyboards/me/cocot38mini/v2/keymaps/sy/keymap.c
@@ -99,10 +99,10 @@ void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
     tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
 
     if (state->pressed) {
-        if (state->count == 1
-#ifndef PERMISSIVE_HOLD
-            && !state->interrupted
-#endif
+        if (state->count == 1 && !state->interrupted
+// #ifndef PERMISSIVE_HOLD
+            // && !state->interrupted
+// #endif
         ) {
             register_code16(tap_hold->hold);
             tap_hold->held = tap_hold->hold;
